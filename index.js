25d690f5f3f0a89591ef2864028ac0883725d1eb
edgar <calderas@gmail.com>
2016-02-10 20:25:32 -0500

    cleanup

--- index.js
+++ index.js
@@ -12,9 +12,14 @@ var AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY;
 var AWS_SECRET_KEY = process.env.AWS_SECRET_KEY;
 var S3_BUCKET = process.env.S3_BUCKET;
 
+aws.config.update({
+  accessKeyId: AWS_ACCESS_KEY,
+  secretAccessKey: AWS_SECRET_KEY
+});
+var awsS3 = new aws.S3();
+
 function uploadToS3(image, fileName) {
   console.log("uploading to S3", fileName);
-  var s3 = awsConfig();
   var s3_params = {
     Bucket: S3_BUCKET,
     Key: fileName,
@@ -23,7 +28,7 @@ function uploadToS3(image, fileName) {
     ContentType: "image/jpg",
     ACL: 'public-read'
   };
-  s3.upload(s3_params, function(err, data) {
+  awsS3.upload(s3_params, function(err, data) {
     if (err) {
       console.log("Error uploading data: ", err);
     } else {
@@ -32,25 +37,16 @@ function uploadToS3(image, fileName) {
   });
 }
 
-function getList(cb) {
-  var s3 = awsConfig();
+function getS3List(cb) {
   var params = {
     Bucket: S3_BUCKET
   };
-  s3.listObjects(params, function(err, data) {
+  awsS3.listObjects(params, function(err, data) {
     if (err) console.log(err, err.stack);
     else {
       cb(data);
     }
   });
-}
-
-function awsConfig() {
-  aws.config.update({
-    accessKeyId: AWS_ACCESS_KEY,
-    secretAccessKey: AWS_SECRET_KEY
-  });
-  return new aws.S3();
 }
 
 function mailParserFn(email) {
@@ -63,7 +59,6 @@ function mailParserFn(email) {
     if (mail_object.attachments) {
       console.log("attachments:", mail_object.attachments.length);
       mail_object.attachments.forEach(function(attachment) {
-        console.log("attachment");
         var name = new Date(mail_object.date).toISOString() + ".jpeg";
         uploadToS3(attachment.content, name);
       });
@@ -87,28 +82,29 @@ function uploadRequest(request, response) {
   return;
 }
 
+function parseS3list(data) {
+  data.Contents.forEach(function(item){
+    item.date = item.Key.split(".jpeg")[0];
+  });
+
+  var images = data.Contents.sort(function(a,b){
+    return new Date(b.date) - new Date(a.date);
+  });
+
+  return images.splice(0, 15);
+}
+
 app.set('port', (PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine());
 
-
 app.get('/', function(request, response) {
-  getList(function(data) {
-
-    data.Contents.forEach(function(item){
-      item.date = item.Key.split(".jpeg")[0];
-    });
-
-    var images = data.Contents.sort(function(a,b){
-      return new Date(b.date) - new Date(a.date);
-    });
-    images = images.splice(0, 15);
-
+  getS3List(function(list) {
     var data = {
       bucket: S3_BUCKET,
-      images: images
+      images: parseS3list(list)
     };
     response.render('index', {data: data});
   });
